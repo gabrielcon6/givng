3a389334f8ff6877bb6b995d02546c3f00040916
Phfa <phfa@outlook.com.au>
2019-12-10 20:45:46 +1100

    fixed ruby routes

--- givng_server/config/routes.rb
+++ givng_server/config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :groups
   resources :givngs
   resources :users
+  resources :sessions
+  root :to => 'users#new'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
