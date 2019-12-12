c9af7d2a19e7a0adc3b031b1be4fa89e7bcea17c
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-12-13 10:50:05 +1100

    adding givng list

--- givng_app/src/components/GivingList.js
+++ givng_app/src/components/GivingList.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Givngs extends Component {
-  render() {
-    return (
-      <div>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-        <h1>Your existing Givings:</h1>
-      </div>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+          givngList: []
+        };
+      }
+    
+      componentDidMount() {
+        const serverURL = `http://localhost:3005/givngs.json`;
+        axios.get(serverURL).then(res => {
+          const myData = res.data;
+          this.setState({
+            givngList: myData
+          });
+        });
+      }
+    
+      render() {
+        const myGivngList = this.state.givngList.map((givng, index) => (
+          <ul key={index}>
+            <li>Givng: {givng.name}</li>
+          </ul>
+        ));
+    
+        return (
+          <div>
+              <br />
+              <br />
+              <br />
+              <br />
+              <h1>Your existing Givngs:</h1>
+              <div>{myGivngList}</div>
+          </div>
+        );
+      }
+    }
 
 export default Givngs;
