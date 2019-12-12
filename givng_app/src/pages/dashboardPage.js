bda48cd05e9abd22da2a5cee0e2cb036535f240c
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-12-13 10:13:01 +1100

    refactored dashboard

--- givng_app/src/pages/dashboardPage.js
+++ givng_app/src/pages/dashboardPage.js
@@ -2,20 +2,9 @@ import React, { Component } from "react";
 import Carousels from "../components/Carousels";
 
 class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      current_user: 0
-    };
-  }
   render() {
     return (
       <div>
-        {/* <h1> What about Givng?</h1>
-        <br />
-        <br />
-        <br />
-        <h1> Givng to your loved ones is Givng to yourself, twice!</h1> */}
         <Carousels />
       </div>
     );
