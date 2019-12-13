4dc0717fa61a28925eb2b2b8922eaf9a04cde16f
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-12-13 12:22:57 +1100

    including cards for givngs

--- givng_app/src/components/GivingList.js
+++ givng_app/src/components/GivingList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Card, CardDeck } from "react-bootstrap";
+
 
 class Givngs extends Component {
     constructor(props) {
@@ -22,9 +24,21 @@ class Givngs extends Component {
     
       render() {
         const myGivngList = this.state.givngList.map((givng, index) => (
-          <ul key={index}>
-            <li>{givng.name}</li>
-          </ul>
+            <CardDeck key={index}>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>{givng.name}</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardDeck>
         ));
     
         return (
