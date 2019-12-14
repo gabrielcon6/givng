81a609f73a292311ec0f2346b260d84a5018c95f
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-12-14 13:37:06 +1100

    cleaned dead code

--- givng_app/src/components/App.js
+++ givng_app/src/components/App.js
@@ -45,21 +45,6 @@ export default class App extends React.Component {
       });
   }
 
-  //   axios.post(`http://localhost:3000/api/users`, {
-  //     user: {
-  //         name: 'Aaron Cox',
-  //         email: 'aaron@ga.com.au',
-  //         password: 'abc123',
-  //         password_confirmation: 'abc123'
-  //     }
-  // }).then(response => {
-  //     console.log('Successfully created user: ')
-  //     console.log(response);
-  // }).catch(err => {
-  //     console.log('Failed to create user with error: ')
-  //     console.log(err);
-  // });
-
   handleLogin(email, password) {
     axios
       .post(`${SERVER_URL}/api/auth/get_token`, {
