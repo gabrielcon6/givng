448286efde50cda3d3284276e81c1fb56b87b6b0
Chen He <chenhe.elec@gmail.com>
2019-12-14 13:25:30 +1100

    fix login issue

--- givng_app/src/components/navbar/NavBar.js
+++ givng_app/src/components/navbar/NavBar.js
@@ -1,52 +1,49 @@
-import React, { Component } from 'react';
-import { Link } from "@reach/router"
-import { Modal, Button, ButtonToolbar} from "react-bootstrap";
-import RegisterForm from '../RegisterForm';
-import { navigate } from '@reach/router';
-import axios from 'axios';
-import { StyledNav, StyledNavLinks, LogoImg} from './StyledNavBar'
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+import { Modal, Button, ButtonToolbar } from "react-bootstrap";
+import RegisterForm from "../RegisterForm";
+import { navigate } from "@reach/router";
+import axios from "axios";
+import { StyledNav, StyledNavLinks, LogoImg } from "./StyledNavBar";
 
 class LoginModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        email: '',
-        password: ''
+      email: "",
+      password: ""
     };
-}
+  }
 
-onLoginClick(data) {
-  axios
-  .post(`http://localhost:3005/api/auth/get_token`, {
-    user: {
-      email: data.email,
-      password: data.password,
-    }
-  })
-  .then(response => {
-    // TODO: use a toast service, or modal or something
-    // better than an allert.
-    alert("user successfully created, please login");
-    // Navigate to the home page.
-    navigate("/");
-    console.log(response);
-  }).catch(err => {
-    console.error(err);
-  })
+  onLoginClick() {
+    axios
+      .post(`http://localhost:3005/api/auth/get_token`, {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        // TODO: use a toast service, or modal or something
+        // better than an allert.
+        alert("Login");
+        // Navigate to the home page.
+        navigate("/");
+        console.log(response);
+        console.log(response.data.token);
+        sessionStorage.setItem("auth", JSON.stringify(response.data));
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
 
-    // TODO: validate inputs
-    this.props.login(this.state.email, this.state.password);
-    console.log(this.props.login);
-}
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
 
-handleEmailChange = (e) => {
-    this.setState({email: e.target.value});
-}
-
-handlePasswordChange = (e) => {
-    this.setState({password: e.target.value});
-}
-    render(){
+  handlePasswordChange = e => {
+    this.setState({ password: e.target.value });
+  };
+  render() {
     return (
       <Modal
         {...this.props}
@@ -56,24 +53,39 @@ handlePasswordChange = (e) => {
       >
         <Modal.Header closeButton />
         <Modal.Body>
-        <div className="container">
-                <h1>Login</h1>
+          <div className="container">
+            <h1>Login</h1>
 
-                
-                <label htmlFor="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="email" required
-                    value={this.state.email} onChange={this.handleEmailChange}  
-                    />
+            <label htmlFor="email">
+              <b>Email</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              required
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+            />
 
-                <label htmlFor="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required 
-                    value={this.state.password} onChange={this.handlePasswordChange} 
-                    />
+            <label htmlFor="psw">
+              <b>Password</b>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="psw"
+              required
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+            />
 
-                <div className="clearfix">
-                    <button className="loginBtn" onClick={() => this.onLoginClick() }>Login Up</button>
-                </div>
+            <div className="clearfix">
+              <button className="loginBtn" onClick={() => this.onLoginClick()}>
+                Login Up
+              </button>
             </div>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.props.onHide}>Close</Button>
@@ -101,16 +113,16 @@ class SignUpModal extends Component {
         // Navigate to the home page.
         navigate("/");
         console.log(response);
-      }).catch(err => {
-        console.error(err);
       })
-
+      .catch(err => {
+        console.error(err);
+      });
   }
-handleCancelSignup() {
+  handleCancelSignup() {
     // Navigate to the home page.
-    navigate('/');
-}
-    render(){
+    navigate("/");
+  }
+  render() {
     return (
       <Modal
         {...this.props}
@@ -120,13 +132,12 @@ handleCancelSignup() {
       >
         <Modal.Header closeButton />
         <Modal.Body>
-        <div className="container">
+          <div className="container">
             <RegisterForm
-                onSignup={(data) => this.handleSignup(data)}
-                onCancelClick={() => this.handleCancelSignup()}
-            >               
-            </RegisterForm>
-            </div>
+              onSignup={data => this.handleSignup(data)}
+              onCancelClick={() => this.handleCancelSignup()}
+            ></RegisterForm>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.props.onHide}>Close</Button>
@@ -135,19 +146,17 @@ handleCancelSignup() {
     );
   }
 }
-  
+
 class LoginModalTrigger extends Component {
   state = {
-    show: false,
-  }
+    show: false
+  };
   render() {
     return (
       <ButtonToolbar>
-        <div onClick={() => this.setState({ show: true })}>
-          Login
-        </div>
-  
-        < LoginModal
+        <div onClick={() => this.setState({ show: true })}>Login</div>
+
+        <LoginModal
           show={this.state.show}
           onHide={() => this.setState({ show: false })}
         />
@@ -155,19 +164,17 @@ class LoginModalTrigger extends Component {
     );
   }
 }
- 
+
 class SignUpModalTrigger extends Component {
   state = {
-    show: false,
-  }
+    show: false
+  };
   render() {
     return (
       <ButtonToolbar>
-        <div onClick={() => this.setState({ show: true })}>
-          Sign Up
-        </div>
-  
-        < SignUpModal
+        <div onClick={() => this.setState({ show: true })}>Sign Up</div>
+
+        <SignUpModal
           show={this.state.show}
           onHide={() => this.setState({ show: false })}
         />
@@ -177,46 +184,51 @@ class SignUpModalTrigger extends Component {
 }
 
 export default class NavBar extends Component {
-  
-    handleLogoutClick(event) {
-        event.preventDefault();
-        this.props.user.logout();
-    }
+  handleLogoutClick(event) {
+    event.preventDefault();
+    this.props.user.logout();
+  }
 
-    render() {
+  render() {
+    const user = this.props.user;
 
-        const user = this.props.user;
-    
-        return (
-            <div>
-                <Link to="/"><LogoImg src={"./givng_logo.png"}/></Link>
-                <StyledNav>
+    return (
+      <div>
+        <Link to="/">
+          <LogoImg src={"./givng_logo.png"} />
+        </Link>
+        <StyledNav>
+          {!user.isLoggedIn && (
+            <>
+              {/* <StyledNavLinks style={{float:'right'}}><Link to="/">Login</Link></StyledNavLinks> */}
+              {/* <StyledNavLinks style={{float:'right'}}><Link to="/register">Register</Link></StyledNavLinks> */}
+              <StyledNavLinks style={{ float: "right" }}>
+                <SignUpModalTrigger />
+              </StyledNavLinks>
+              <StyledNavLinks style={{ float: "right" }}>
+                <LoginModalTrigger />
+              </StyledNavLinks>
+            </>
+          )}
 
-                    { !user.isLoggedIn &&
-                        <>
-                            {/* <StyledNavLinks style={{float:'right'}}><Link to="/">Login</Link></StyledNavLinks> */}
-                            {/* <StyledNavLinks style={{float:'right'}}><Link to="/register">Register</Link></StyledNavLinks> */}
-                            <StyledNavLinks style={{float:'right'}}><SignUpModalTrigger /></StyledNavLinks>
-                            <StyledNavLinks style={{float:'right'}}><LoginModalTrigger /></StyledNavLinks>
-                        </>
-                    }
-                    
-
-                    { user.isLoggedIn && user.currentUser && 
-                    <>
-                      <StyledNavLinks>Special Dates</StyledNavLinks>
-                      <StyledNavLinks>Favourite Friends</StyledNavLinks>
-                      <StyledNavLinks>Logout</StyledNavLinks>
-                      <li style={{float:'right', marginRight: '5px'}}>
-                          <span className="app-nav-user-info">
-                              {user.currentUser.name}<br />
-                              <a href="#" onClick={(e)=>this.handleLogoutClick(e)}>logout</a>
-                          </span>
-                      </li>
-                    </>
-                    }
-                </StyledNav>
-            </div>
-        );
-    }
+          {user.isLoggedIn && user.currentUser && (
+            <>
+              <StyledNavLinks>Special Dates</StyledNavLinks>
+              <StyledNavLinks>Favourite Friends</StyledNavLinks>
+              <StyledNavLinks>Logout</StyledNavLinks>
+              <li style={{ float: "right", marginRight: "5px" }}>
+                <span className="app-nav-user-info">
+                  {user.currentUser.name}
+                  <br />
+                  <a href="#" onClick={e => this.handleLogoutClick(e)}>
+                    logout
+                  </a>
+                </span>
+              </li>
+            </>
+          )}
+        </StyledNav>
+      </div>
+    );
+  }
 }
