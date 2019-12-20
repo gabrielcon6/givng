fdae088f026b17a4d9e3a514f6712be6362fd2c3
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-12-21 10:09:43 +1100

    added image background

--- web/src/components/GivngDetail.js
+++ web/src/components/GivngDetail.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyledContainer, Title, H2, H3 } from "../styles/StyledGivngDetailList";
 // import EditGivng from "./EditGivng"
 // import { Title } from "../styles/StyledHomeContent";
+import { CardImage } from "../styles/StyledGivngList.js"
 
 class GivngDetail extends React.Component {
 
@@ -10,13 +11,17 @@ class GivngDetail extends React.Component {
     console.log(this.props);
     const givng = this.props.givng;
     return (
+      <>
+      <CardImage className={`${givng.theme}`} style={{height: "50vh"}}></CardImage>
+
       <StyledContainer>
-        <Title>{givng.name}</Title>
+        <Title style={{marginTop: "-6vh"}}>{givng.name}</Title>
         <H2>Total Budget: $ {givng.budget}0</H2>
         {/* <EditGivng givng={givng.id}/> */}
         <H3>Your groups:</H3>
       </StyledContainer>
       
+      </>
     );
   }
 }
